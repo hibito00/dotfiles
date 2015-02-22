5f1d44b82def7d4c5e06ed0f4b65bdc6837a163b
hibito00 <hibito00@www3145ue.sakura.ne.jp>
2015-02-22 14:22:40 +0900

    delete vim directory

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,2 +1,5 @@
 ln -sf ~/dotfiles/vimrc ~/.vimrc
-ln -sf ~/dotfiles/vim ~/.vim
+if [ ! -e ~/.vim/bundle/ ]; then
+  mkdir ~/.vim/bundle
+  git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+fi
